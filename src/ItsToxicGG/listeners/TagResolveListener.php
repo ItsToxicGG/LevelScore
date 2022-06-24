24e1b4523fa5be948a5333f4219bc64654e2c2fc
ItsToxic <95104544+ItsToxicGG@users.noreply.github.com>
2022-06-24 21:59:45 +0200

    ?

--- src/ItsToxicGG/listeners/TagResolveListener.php
+++ src/ItsToxicGG/listeners/TagResolveListener.php
@@ -1,8 +1,42 @@
 <?php
+declare(strict_types = 1);
 
 namespace ItsToxicGG\listeners;
 
-class TagResolveListener 
-{
-  // soon?
+use ItsToxicGG\Main;
+use Ifera\ScoreHud\event\TagsResolveEvent;
+use pocketmine\event\Listener;
+use function count;
+use function explode;
+
+class TagResolveListener implements Listener{
+
+	/** @var Main */
+	private $plugin;
+
+	public function __construct(Main $plugin){
+		$this->plugin = $plugin;
+	}
+
+	public function onTagResolve(TagsResolveEvent $event){
+		$tag = $event->getTag();
+    $player = $event->getPlayer();
+		$tags = explode('.', $tag->getName(), 2);
+		$value = "";
+
+		if($tags[0] !== 'levelsystem' || count($tags) < 2){
+			return;
+		}
+
+		switch($tags[1]){
+			case "lvl":
+        $value = $this->plugin->levelsystem->getLevel($player);
+				break;
+      case "level":
+        $value = $this->plugin->levelsystem->getLevel($player);
+        break;
+		}
+
+		$tag->setValue((string) $value);
+	}
 }
