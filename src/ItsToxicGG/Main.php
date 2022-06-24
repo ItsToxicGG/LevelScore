c73d1e376498aedab31aaf2caa96b73b87f79e38
ItsToxic <95104544+ItsToxicGG@users.noreply.github.com>
2022-06-24 21:50:34 +0200

    progress has begin?

--- src/ItsToxicGG/Main.php
+++ src/ItsToxicGG/Main.php
@@ -3,10 +3,13 @@
 namespace ItsToxicGG;
 
 use pocketmine\plugin\PluginBase;
+use ItsToxicGG\listeners\EventListener;
+use ItsToxicGG\listeners\TagResolveListener;
 
 class Main extends PluginBase {
 
   protected function onEnable(): void{
-    // soon!
+		$this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+		$this->getServer()->getPluginManager()->registerEvents(new TagResolveListener($this), $this);
   }
 } 
