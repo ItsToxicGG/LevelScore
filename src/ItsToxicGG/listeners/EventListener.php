10503198b798457cbcec94a4aaa0b19052fcc5ce
ItsToxic <95104544+ItsToxicGG@users.noreply.github.com>
2022-06-24 22:17:17 +0200

    hmm?

--- src/ItsToxicGG/listeners/EventListener.php
+++ src/ItsToxicGG/listeners/EventListener.php
@@ -2,8 +2,32 @@
 
 namespace ItsToxicGG\Listeners;
 
+use ItsToxicGG\Main;
+use Ifera\ScoreHud\event\PlayerTagUpdateEvent;
+use Ifera\ScoreHud\scoreboard\ScoreTag;
 use pocketmine\event\Listener;
+use pocketmine\player\Player;
 
 class EventListener implements Listener {
   
+  /** @var Main */
+	private $plugin;
+
+	public function __construct(Main $plugin){
+		$this->plugin = $plugin;
+	}
+ 
+  public function onLevelChange(){ // not a real event
+    $username = $player->getName();
+    
+		if(is_null($username)){
+			return;
+		}
+
+		$player = $this->plugin->getServer()->getPlayerByPrefix($username);
+
+		if($player instanceof Player && $player->isOnline()){
+			(new PlayerTagUpdateEvent($player, new ScoreTag("levelsystem.lvl", (string) $this->plugin->Levelsystem->getDataManager()->getLevel();
+		}
+  }
 }
