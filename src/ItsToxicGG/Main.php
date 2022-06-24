3b0e11bc863655d9aa850de35e005812ba040808
ItsToxic <95104544+ItsToxicGG@users.noreply.github.com>
2022-06-24 21:43:28 +0200

    Update Main.php

--- src/ItsToxicGG/Main.php
+++ src/ItsToxicGG/Main.php
@@ -5,5 +5,8 @@ namespace ItsToxicGG;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase {
-// I'm on mobile when I reach on pc ill start progress
+
+  protected function onEnable(): void{
+    // soon!
+  }
 } 
