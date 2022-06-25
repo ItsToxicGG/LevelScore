fa2d14291af9ef5d5e8d92bba1e7166939bc489a
ItsToxic <95104544+ItsToxicGG@users.noreply.github.com>
2022-06-25 13:40:21 +0200

    ?

--- src/ItsToxicGG/LvlScore.php
+++ src/ItsToxicGG/LvlScore.php
@@ -5,15 +5,15 @@ namespace ItsToxicGG;
 use pocketmine\plugin\PluginBase;
 use ItsToxicGG\listeners\EventListener;
 use ItsToxicGG\listeners\TagResolveListener;
-use Laith98Dev\LevelSystem\Main;
+use Laith98Dev\LevelSystem\DataMgr;
 
 class LvlScore extends PluginBase {
 
-  /** @var Main */	
+  /** @var DataMgr */	
   public $Levelsystem;
 
   protected function onEnable(): void{
-	        $this->Levelsystem = $this->getServer()->getPluginManager()->getPlugin("LevelSystem");
+	        $this->Levelsystem = new DataMgr($this);
 		$this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
 		$this->getServer()->getPluginManager()->registerEvents(new TagResolveListener($this), $this);
   }
